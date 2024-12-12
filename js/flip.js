6003801f97dd573a0771c28ab5700c2bc2f5011c
손민식[surajang] <memswap@gmail.com>
2024-12-12 10:43:26 +0900

    feat: add map

--- js/flip.js
+++ js/flip.js
@@ -1,3 +1,5 @@
+var map = null;
+
 document.addEventListener("DOMContentLoaded", function () {
   var index = 1;
   function foo() {
@@ -16,3 +18,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setInterval(foo, 1000);
 });
+
+function initMap() {
+  map = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(37.5141299, 127.0291812),
+      zoom: 10
+  });
+}
+
+window.navermap_authFailure = function () {
+  console.log("네이버 지도 API 인증에 실패했습니다.");
+}
